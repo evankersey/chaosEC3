8e7ae5e6680a74074b7660a2d75146b05a71aa59
Evan Kersey <47676456+evankersey@users.noreply.github.com>
2019-04-26 17:56:57 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,13 +4,21 @@
 // Evan Kersey
 //
 // description: the chaos game creates pictures of text by jumping partway between a given set of points randomly
+//      OUTPUT goes to output.txt
+//
+//      example output is in the outputFiles folder
 //
 // Special Features.
-//  classes, polygon of any size, rotation of polygon, text output, etc...
+//  classes, polygon of any size, rotation of polygon, text output, option to not repeat previous node.
 //
 // ToDo
 //  allow program to take arguments from command line
-//  do not repeat previous node
+//
+
+// completed step 10
+// all step 11
+// extra features
+
 
 #include <iostream>
 #include<vector>
@@ -23,13 +31,15 @@
 using namespace std;
 
 ////////////////CHANGE THESE VARIABLES//////////
-double theta = 0; //in radians
+double theta = .3; //in radians
 int numVertices = 5; //how many vertices the polygon has
-double divideByMe = 2.5; //changes where the newpoint is calculated. divide by two for midpoint
+double percentToVertex = .5; //changes where the newpoint is calculated. multiply by .5 for midpoint
+bool doNotRepeatPoint = true; //
 ////////////////BE CAREFUL//////////////////////
-const int maxWidth = 150;
-const int maxHeight = 150;
-int numIterations = 2000000;
+//these tend to produce a good image at higher values but can also crash the program
+const int maxWidth = 200;
+const int maxHeight = 200;
+int numIterations = 100000;
 ///////////////////////////////////////////////
 bool chaosBoard[maxHeight][maxWidth] = {};
 
@@ -39,10 +49,19 @@ struct point
     int y;
 };
 
-void setPointBoardTrue(point* setPoint) //sets point on chaosBoard to true ("X ")
+void setPointBoardTrue(point* setPoint)   //sets point on chaosBoard to true ("X ")
 {
+    static int nbrPointsOutOfBounds = 0;
+    if ((setPoint->x > maxHeight) || (setPoint->y > maxWidth) ||
+            (setPoint->x < 0) || (setPoint->y < 0))
+    {
+        cout << "setPointBoardTrue: " << setPoint->x << " " << setPoint->y << endl;
+        nbrPointsOutOfBounds++;
+        cout << "OUT OF BOUNDS! " << nbrPointsOutOfBounds << endl;
+        return;
+    }
     chaosBoard[setPoint -> x][setPoint -> y] = true;
-}
+}//end setBoardTrue
 
 void drawGrid()//prints grid to terminal
 {
@@ -63,14 +82,29 @@ void drawGrid()//prints grid to terminal
 point* findNextPoint(point* currPoint, vector<polygon::point> startPoints) //finds next point to print. randomly picks a startpoint and moves halfway to it
 {
     int randomPoint;
-    randomPoint = rand() % startPoints.size();
+
+    if(doNotRepeatPoint)//disallows generating the same number twice
+    {
+        static int previousPoint = -1;
+        do
+        {
+            randomPoint = rand() % startPoints.size();
+
+        }
+        while(randomPoint == previousPoint);
+        previousPoint = randomPoint;
+    }
+    else
+        randomPoint = rand() % startPoints.size(); //or just randomly selects a new point, can repeat
 
     point* newPoint = new point;
-    newPoint->x = (currPoint->x + startPoints[randomPoint].x)/divideByMe;
-    newPoint->y = (currPoint->y + startPoints[randomPoint].y)/divideByMe;
+    newPoint->x = currPoint->x + (-currPoint->x + startPoints[randomPoint].x)*percentToVertex;
+    newPoint->y = currPoint->y + (-currPoint->y + startPoints[randomPoint].y)*percentToVertex;
 
     return newPoint;
 }//end find Next Point
+
+
 
 void printGrid()//prints grid to outputFile
 {
@@ -107,6 +141,7 @@ int main()
         setPointBoardTrue(currentPoint);
         //drawGrid();
         currentPoint = findNextPoint(currentPoint, myPolygon.vertices);
+        cout << i << endl;
     }
     printGrid();
     return 0;
